df9505e0274ec644d2bc1a4e54c8d81689600751
DeAleon <donnotel@mail.ru>
2024-07-06 22:23:27 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -3,16 +3,18 @@ primes = []
 not_primes  = []
 numbers.remove(1)
 for i in numbers:
-    count = 0
+    #count = 0
+    is_primes = True
     for j in range(2, i):
         if i % j == 0 and i != j:
-            count += 1
-    if count == 0:
+            #count += 1
+            is_primes = False
+    #if count == 0:
+    if is_primes:
         primes.append(i)
     else:
         not_primes.append(i)
 print(primes)
 print(not_primes)
-
 
 
